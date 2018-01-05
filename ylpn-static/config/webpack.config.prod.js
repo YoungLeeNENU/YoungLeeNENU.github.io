0ea50a9eae8d9accda97ca063ccb5e5f8d702360
YoungLee <youngleemails@gmail.com>
2018-01-05 20:05:48 +0800

    deploy

--- ylpn-static/config/webpack.config.prod.js
+++ ylpn-static/config/webpack.config.prod.js
@@ -12,6 +12,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
+const PrajnaWrapperPlugin = require('prajna-wrapper-plugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -329,6 +330,14 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new PrajnaWrapperPlugin({
+      includes: ['./public/index.html'],
+      options: {
+        autopv: true,
+        env: 'product',
+        project: 'ylpn'
+      }
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
